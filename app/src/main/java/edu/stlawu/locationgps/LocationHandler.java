06131bedd24091d3b7974f78b36e206d0439f68d
vitzthumar <vitzthum.ar@gmail.com>
2018-10-09 15:24:02 -0400

    firstt commit

---
+++ app/src/main/java/edu/stlawu/locationgps/LocationHandler.java
@@ -0,0 +1,96 @@
+package edu.stlawu.locationgps;
+
+import android.Manifest;
+import android.content.Context;
+import android.content.pm.PackageManager;
+import android.hardware.Sensor;
+import android.hardware.SensorEvent;
+import android.hardware.SensorEventListener;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
+import android.os.Bundle;
+
+import java.util.Observable;
+
+public class LocationHandler
+    extends Observable
+    implements LocationListener {
+
+    // handles location services for the device
+    private LocationManager lm;
+    private MainActivity act;
+
+    public LocationHandler(MainActivity act) {
+        this.act = act;
+        this.lm = (LocationManager) this.act.getSystemService(
+                Context.LOCATION_SERVICE);
+
+        if (this.act.checkSelfPermission(
+                Manifest.permission.ACCESS_FINE_LOCATION)
+                == PackageManager.PERMISSION_GRANTED) {
+            lm.requestLocationUpdates(
+                    LocationManager.GPS_PROVIDER,
+                    5000,
+                    0,
+                    this);
+            lm.requestLocationUpdates(
+                    LocationManager.NETWORK_PROVIDER,
+                    5000,
+                    0,
+                    this);
+            lm.requestLocationUpdates(
+                    LocationManager.PASSIVE_PROVIDER,
+                    5000,
+                    0,
+                    this);
+
+            // check for initial GPS coordinate
+            Location l =
+                lm.getLastKnownLocation(LocationManager.GPS_PROVIDER);
+
+            if (l != null) {
+                setChanged();
+                notifyObservers(l);
+                return;
+            }
+
+            l = lm.getLastKnownLocation(LocationManager.NETWORK_PROVIDER);
+            if (l != null) {
+                setChanged();
+                notifyObservers(l);
+                return;
+            }
+
+            l = lm.getLastKnownLocation(
+                    LocationManager.PASSIVE_PROVIDER);
+            if (l != null) {
+                setChanged();
+                notifyObservers(l);
+                return;
+            }
+
+        }
+    }
+
+    @Override
+    public void onLocationChanged(Location location) {
+        setChanged();
+        notifyObservers(location);
+    }
+
+    @Override
+    public void onStatusChanged(String s, int i, Bundle bundle) {
+
+    }
+
+    @Override
+    public void onProviderEnabled(String s) {
+
+    }
+
+    @Override
+    public void onProviderDisabled(String s) {
+
+    }
+}
---
+++ app/src/main/java/edu/stlawu/locationgps/MainActivity.java
@@ -0,0 +1,100 @@
+package edu.stlawu.locationgps;
+
+import android.Manifest;
+import android.content.pm.PackageManager;
+import android.location.Location;
+import android.location.LocationManager;
+import android.support.annotation.NonNull;
+import android.support.v4.app.ActivityCompat;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.widget.TextView;
+
+import java.util.Observable;
+import java.util.Observer;
+
+public class MainActivity
+        extends AppCompatActivity
+        implements Observer {
+
+    private TextView tv_lat;
+    private TextView tv_lon;
+
+    private Observable location;
+    private LocationHandler handler = null;
+    private final static int PERMISSION_REQUEST_CODE = 999;
+
+    private boolean permissions_granted;
+    private final static String LOGTAG =
+            MainActivity.class.getSimpleName();
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        this.tv_lat = findViewById(R.id.tv_lat);
+        this.tv_lon = findViewById(R.id.tv_lon);
+
+        if (handler == null) {
+            this.handler = new LocationHandler(this);
+            this.handler.addObserver(this);
+        }
+
+
+        // check permissions
+        if (checkSelfPermission(
+                Manifest.permission.ACCESS_FINE_LOCATION)
+                != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(
+                    this,
+                    new String[] { Manifest.permission.ACCESS_FINE_LOCATION },
+                    PERMISSION_REQUEST_CODE
+            );
+        }
+    }
+
+    public boolean isPermissions_granted() {
+        return permissions_granted;
+    }
+
+    @Override
+    public void onRequestPermissionsResult(
+        int requestCode, @NonNull String[] permissions,
+            @NonNull int[] grantResults) {
+
+        super.onRequestPermissionsResult(
+                requestCode, permissions, grantResults);
+
+        if (requestCode == PERMISSION_REQUEST_CODE) {
+            // we have only asked for FINE LOCATION
+            if (grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                this.permissions_granted = true;
+                Log.i(LOGTAG, "Fine location permisssion granted.");
+            }
+            else {
+                this.permissions_granted = false;
+                Log.i(LOGTAG, "Fine location permisssion not granted.");
+            }
+        }
+
+    }
+
+    @Override
+    public void update(Observable observable,
+                       Object o) {
+        if (observable instanceof LocationHandler) {
+            Location l = (Location) o;
+            final double lat = l.getLatitude();
+            final double lon = l.getLongitude();
+
+            runOnUiThread(new Runnable() {
+                @Override
+                public void run() {
+                    tv_lat.setText(Double.toString(lat));
+                    tv_lon.setText(Double.toString(lon));
+                }
+            });
+        }
+    }
+}
