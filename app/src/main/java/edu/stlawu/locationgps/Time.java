b4db24c1192ec4b82020a789eec58589a09c0aad
Nevaan Perera <nevaan9@gmail.com>
2018-10-14 19:58:34 -0400

    added a time class to get our times converted

---
+++ app/src/main/java/edu/stlawu/locationgps/Time.java
@@ -0,0 +1,20 @@
+package edu.stlawu.locationgps;
+
+import java.util.Date;
+
+/**
+ * Purpse of this class is to take in date and time of an event and convert it into either seconds, minutes or hours.
+ */
+public class Time {
+    public static double convertToSeconds (Date dateOb1, Date dateOb2) {
+        return 0.0;
+    }
+
+    public static double convertToHours (Date dateOb1, Date dateOb2) {
+        return 0.0;
+    }
+
+    public static double convertToMinutes (Date dateOb1, Date dateOb2) {
+        return 0.0;
+    }
+}
